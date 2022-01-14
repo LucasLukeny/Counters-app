0de41196155a89d2994d8827c0d656ef24a93f64
Lucas Lukeny <lucas.barreto@wellowgroup.com>
2022-01-14 12:35:58 +0000

    Final Version

---
+++ src/components/Counters.jsx
@@ -0,0 +1,50 @@
+import React, { Component } from "react";
+import Counter from "./Counter";
+
+class Counters extends React.Component {
+  state = {
+    counters: [
+      { id: 1, value: 0 },
+      { id: 2, value: 0 },
+      { id: 3, value: 0 },
+      { id: 4, value: 0 },
+    ],
+  };
+
+  handleReset = () => {
+    const counters = this.state.counters.map((c) => {
+      c.value = 0;
+      return c;
+    });
+    this.setState({ counters });
+  };
+
+  handleDelete = (counterId) => {
+    const counters = this.state.counters.filter((c) => c.id !== counterId);
+    this.setState({ counters });
+  };
+
+  render() {
+    const { onReset, counters, onDelete, onIncrement } = this.props;
+    console.log("Counter - Rendered");
+    return (
+      <div>
+        <button onClick={onReset} className="btn btn-primary btn-sm m-2">
+          Reset
+        </button>
+        {counters.map((counter) => (
+          <Counter
+            key={counter.id}
+            onDelete={onDelete}
+            onIncrement={onIncrement}
+            counter={counter}
+          >
+            <h4>Counter #{counter.id}</h4>
+          </Counter>
+        ))}
+      </div>
+    );
+  }
+}
+
+export default Counters;
